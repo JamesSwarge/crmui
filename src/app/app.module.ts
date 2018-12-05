89ee50ae55801175bf04c94ef45432ccf91cba9d
JamesSwarge <james.swarge@gmail.com>
2018-12-05 15:33:02 +0530

    added add-user component within dashboard & the material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddUserComponent } from './dashboard/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { MatMenuModule, MatSidenavModule, MatTabsModule, MatListModule, MatToolbarModule, MatIconModule, MatButtonModule, MatCardModule, MatInputModule, MatExpansionModule, MatDatepickerModule, MatNativeDateModule, MatSlideToggleModule, MatDividerModule, MatSnackBarModule } from '@angular/material';
+
+@NgModule ({
+    declarations: [],
+    imports: [
+        MatMenuModule,
+        MatSidenavModule,
+        MatTabsModule,
+        MatListModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatExpansionModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSlideToggleModule,
+        MatDividerModule,
+        MatSnackBarModule
+    ],
+    exports: [
+        MatMenuModule,
+        MatSidenavModule,
+        MatTabsModule,
+        MatListModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatExpansionModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatSlideToggleModule,
+        MatDividerModule,
+        MatSnackBarModule
+    ]
+})
+
+export class MaterialModule { }
